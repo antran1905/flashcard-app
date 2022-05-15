76a775f179698739855de130d67872f5af131d6d
An Tran <tranquocan97@gmail.com>
2022-05-15 21:57:45 +0700

    Init Card

--- components/card/index.tsx
+++ components/card/index.tsx
@@ -1,14 +1,28 @@
-import React from "react";
-interface ICard {
+import Link from "next/link";
+import React, { HTMLAttributes } from "react";
+import { overrideTailwindClasses } from "tailwind-override";
+interface ICard extends HTMLAttributes<HTMLDivElement> {
   title: string;
   path: string;
+  desc: string;
 }
 
 export const Card = (props: ICard) => {
-  const { path, title } = props;
+  const { path, title, className, desc } = props;
   return (
-    <div className="border-2 rounded-lg border-white">
-      <div className="text-lg h-12 border-b-2 border-white">{title} &rarr;</div>
-    </div>
+    <Link href={path} passHref>
+      <a href={path} className="group">
+        <div
+          className={overrideTailwindClasses(
+            `border-2 border-primary rounded-lg w-64 h-56 min-w-min cursor-pointe group-hover:border-secondary transition-all duration-200 ${className}`
+          )}
+        >
+          <div className="text-xl h-12 border-b-2 border-primary group-hover:border-secondary transition-all duration-200 flex items-center px-6">
+            {title} &rarr;
+          </div>
+          <div className="p-6 text-2xl">{desc}</div>
+        </div>
+      </a>
+    </Link>
   );
 };
--- tailwind.config.js
+++ tailwind.config.js
@@ -1,10 +1,18 @@
 module.exports = {
-  content: [
-    "./pages/**/*.{js,ts,jsx,tsx}",
-    "./components/**/*.{js,ts,jsx,tsx}",
-  ],
+  purge: ["./pages/**/*.{js,ts,jsx,tsx}", "./components/**/*.{js,ts,jsx,tsx}"],
   theme: {
     extend: {},
+    colors: {
+      primary: "#0ff",
+      secondary: "#ff6bd5",
+    },
+    variants: {
+      extends: {
+        borderColor: ['focus', 'hover', 'group-hover'],
+        textColor: ['group-hover'],
+        translate: ['group-hover', 'hover'],
+      }
+    },
   },
   plugins: [],
 }
